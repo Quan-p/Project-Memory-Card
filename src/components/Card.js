534f343f8c2dbe4cedc9c126be72c2bb02f189d0
Quan-p <quanp155@gmail.com>
2021-12-08 17:34:55 -0600

    Object array of all 15 cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,3 +14,76 @@ import Pippin from '../images/Pippin.jpg'
 import Sam from '../images/Sam.jpg'
 import Saruman from '../images/Saruman.jpg'
 import Theoden from '../images/Theoden.jpg'
+
+const Container = () => {
+    let imageCards = [
+        {
+            src: Aragorn,
+            title: 'Aragorn',
+        },
+        {
+            src: Boromir,
+            title: 'Boromir',
+        },
+        {
+            src: Elrond,
+            title: 'Elrond',
+        },
+        {
+            src: Faramir,
+            title: 'Faramir',
+        },
+        {
+            src: Frodo,
+            title: 'Frodo',
+        },
+        {
+            src: Galadriel,
+            title: 'Galadriel',
+        },
+        {
+            src: Gandalf,
+            title: 'Gandalf',
+        },
+        {
+            src: Gimli,
+            title: 'Gimli',
+        },
+        {
+            src: Gollum,
+            title: 'Gollum',
+        },
+        {
+            src: Legolas,
+            title: 'Legolas',
+        },
+        {
+            src: Merry,
+            title: 'Merry',
+        },
+        {
+            src: Pippin,
+            title: 'Pippin',
+        },
+        {
+            src: Sam,
+            title: 'Sam',
+        },
+        {
+            src: Saruman,
+            title: 'Saruman',
+        },
+        {
+            src: Theoden,
+            title: 'Theoden',
+        },
+    ]
+
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default Container;
