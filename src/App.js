573ba019e7340bc721d4882c576ce06ae6408c25
Quan-p <quanp155@gmail.com>
2021-12-09 12:58:47 -0600

    Display all 15 images in container

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <div>
       <Score />
+      <Container />
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,87 +1,14 @@
 import Reach, { useState, useEffect } from 'react';
-import Aragorn from '../images/Aragorn.jpg'
-import Boromir from '../images/Boromir.jpg'
-import Elrond from '../images/Elrond.jpg'
-import Faramir from '../images/Faramir.png'
-import Frodo from '../images/Frodo.png'
-import Galadriel from '../images/Galadriel.jpg'
-import Gandalf from '../images/Gandalf.jpg'
-import Gimli from '../images/Gimli.png'
-import Gollum from '../images/Gollum.png'
-import Legolas from '../images/Legolas.jpg'
-import Merry from '../images/Merry.jpg'
-import Pippin from '../images/Pippin.jpg'
-import Sam from '../images/Sam.jpg'
-import Saruman from '../images/Saruman.jpg'
-import Theoden from '../images/Theoden.jpg'
+import { characters } from './characters';
 
-const Container = () => {
-    let imageCards = [
-        {
-            src: Aragorn,
-            title: 'Aragorn',
-        },
-        {
-            src: Boromir,
-            title: 'Boromir',
-        },
-        {
-            src: Elrond,
-            title: 'Elrond',
-        },
-        {
-            src: Faramir,
-            title: 'Faramir',
-        },
-        {
-            src: Frodo,
-            title: 'Frodo',
-        },
-        {
-            src: Galadriel,
-            title: 'Galadriel',
-        },
-        {
-            src: Gandalf,
-            title: 'Gandalf',
-        },
-        {
-            src: Gimli,
-            title: 'Gimli',
-        },
-        {
-            src: Gollum,
-            title: 'Gollum',
-        },
-        {
-            src: Legolas,
-            title: 'Legolas',
-        },
-        {
-            src: Merry,
-            title: 'Merry',
-        },
-        {
-            src: Pippin,
-            title: 'Pippin',
-        },
-        {
-            src: Sam,
-            title: 'Sam',
-        },
-        {
-            src: Saruman,
-            title: 'Saruman',
-        },
-        {
-            src: Theoden,
-            title: 'Theoden',
-        },
-    ]
+const Container = (props) => {
+    const { score, highscore } = props;
 
     return (
         <div>
-           
+           {characters.map((card) => (
+               <img src={card.src} alt={card.title}/>
+           ))}
         </div>
     )
 }
