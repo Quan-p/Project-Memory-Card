57f904eaf47cafc918312cc7749b123bb833a2e1
Quan-p <quanp155@gmail.com>
2021-12-09 15:48:38 -0600

    Shuffle function

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,12 @@ import Reach, { useState, useEffect } from 'react';
 import { characters } from './characters';
 
 const Container = (props) => {
-    const { score, highscore } = props;
+    const shuffleCards = (newArr) => {
+        for (let i = newArr.length - 1; i > 0; i--) {
+            let randomId = Math.floor(Math.random() * i);
+            [newArr[randomId], newArr[i]] = [newArr[i], newArr[randomId]];
+        }
+    }
 
     return (
         <div>
