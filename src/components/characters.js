1572cb4574ed607fd2fb1908b20c70fc8902b570
Quan-p <quanp155@gmail.com>
2021-12-09 12:52:54 -0600

    Created separate file for character objects

---
+++ src/components/characters.js
@@ -0,0 +1,93 @@
+import Aragorn from '../images/Aragorn.jpg'
+import Boromir from '../images/Boromir.jpg'
+import Elrond from '../images/Elrond.jpg'
+import Faramir from '../images/Faramir.png'
+import Frodo from '../images/Frodo.png'
+import Galadriel from '../images/Galadriel.jpg'
+import Gandalf from '../images/Gandalf.jpg'
+import Gimli from '../images/Gimli.png'
+import Gollum from '../images/Gollum.png'
+import Legolas from '../images/Legolas.jpg'
+import Merry from '../images/Merry.jpg'
+import Pippin from '../images/Pippin.jpg'
+import Sam from '../images/Sam.jpg'
+import Saruman from '../images/Saruman.jpg'
+import Theoden from '../images/Theoden.jpg'
+
+export const characters = [
+    {
+        id: 1,
+        src: Aragorn,
+        title: 'Aragorn',
+    },
+    {
+        id: 2,
+        src: Boromir,
+        title: 'Boromir',
+    },
+    {
+        id: 3,
+        src: Elrond,
+        title: 'Elrond',
+    },
+    {
+        id: 4,
+        src: Faramir,
+        title: 'Faramir',
+    },
+    {
+        id: 5,
+        src: Frodo,
+        title: 'Frodo',
+    },
+    {
+        id: 6,
+        src: Galadriel,
+        title: 'Galadriel',
+    },
+    {
+        id: 7,
+        src: Gandalf,
+        title: 'Gandalf',
+    },
+    {
+        id: 8,
+        src: Gimli,
+        title: 'Gimli',
+    },
+    {
+        id: 9,
+        src: Gollum,
+        title: 'Gollum',
+    },
+    {
+        id: 10,
+        src: Legolas,
+        title: 'Legolas',
+    },
+    {
+        id: 11,
+        src: Merry,
+        title: 'Merry',
+    },
+    {
+        id: 12,
+        src: Pippin,
+        title: 'Pippin',
+    },
+    {
+        id: 13,
+        src: Sam,
+        title: 'Sam',
+    },
+    {
+        id: 14,
+        src: Saruman,
+        title: 'Saruman',
+    },
+    {
+        id: 15,
+        src: Theoden,
+        title: 'Theoden',
+    },
+]
