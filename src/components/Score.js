7ad06c78881dc030ba171ac957dd99636d57adfa
Quan-p <quanp155@gmail.com>
2021-12-14 15:28:20 -0600

    Cleaned up unused parts

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,16 +1,1 @@
 import React, { useState } from 'react';
-import Container from './Card';
-
-const Score = () => {
-    const [currentScore, setCurrentScore] = useState(0);
-    const [highScore, setHighScore] = useState(0);
-
-    return (
-        <div>
-            <div>Score = {currentScore} High Score = {highScore}</div>
-        </div>
-    )
-
-}
-
-export default Score;
