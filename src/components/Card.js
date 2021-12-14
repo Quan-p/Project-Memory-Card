777cba892b6ca19e791932fc337190185ef85af1
Quan-p <quanp155@gmail.com>
2021-12-13 23:17:34 -0600

    Moved click logic to container component temporarily

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { characters } from './characters';
 
-const Container = (props) => {
-    const { score, highScore } = props;
-    
+const Container = (props) => {    
     const shuffleCards = (newArr) => {
         for (let i = newArr.length - 1; i > 0; i--) {
             let randomId = Math.floor(Math.random() * i);
@@ -11,21 +9,40 @@ const Container = (props) => {
         }
     }
 
+    const [clickedCard, setClickedCard] = useState([]);
+
     const [cards, setNewCards] = useState(characters)
 
     const handleCardClick = (e) => {
         const cardName = e.target.parentNode.lastChild.textContent;
+        gameLogic(cardName);
         console.log(cardName);
     }
 
+    const gameLogic = (cardName) => {
 
+    }
 
-    useEffect(() => {
-        const newArr = [...characters];
-        shuffleCards(newArr);
-        setNewCards(newArr);
-    }, [score, highScore]);
-// optimization, shuffle if score changes
+    const [currentScore, setCurrentScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    //everytime card is clicked add its id or something to array
+
+    const handleScore = (cardName) => {
+        if (clickedCard.includes(cardName)) {
+            resetScore();
+        } else {
+            const score = currentScore + 1;
+            if (score > highScore) setHighScore(score);
+            setCurrentScore(score);
+        }
+    }
+    
+
+    const resetScore = () => {
+        setClickedCard([]);
+        setCurrentScore(0);
+    }
+
     return (
         <div className={ 'wrapper' }>
             {characters.map((card) => (
--- src/components/Score.js
+++ src/components/Score.js
@@ -4,27 +4,7 @@ import Container from './Card';
 const Score = () => {
     const [currentScore, setCurrentScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
-    const [clickedCard, setClickedCard] = useState([]);
-    //everytime card is clicked add its id or something to array
 
-    const handleScore = (cardName) => {
-        if (clickedCard.includes(cardName)) {
-            resetScore();
-        } else {
-            const score = currentScore + 1;
-            if (score > highScore) setHighScore(score);
-            setCurrentScore(score);
-        }
-    }
-    
-    const handleCard = (cardName) => {
-        setClickedCard((prevArr) => [...prevArr, cardName]);
-    }
-
-    const resetScore = () => {
-        setClickedCard([]);
-        setCurrentScore(0);
-    }
     return (
         <div>
             <div>Score = {currentScore} High Score = {highScore}</div>
