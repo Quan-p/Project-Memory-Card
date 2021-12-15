273c2811cd97557ab1ae1687d8fb56c9ba8c8568
Quan-p <quanp155@gmail.com>
2021-12-15 15:59:44 -0600

    Styled text and background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,13 @@ const HeaderWrapper = styled.header`
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 60px;
+    padding: 20px;
+    border-bottom: 10px double #b4ba5c;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: #3f411c;
+    color: #ccc7b3;
+    background-color: #f4ece4;
 `
 
 export default Header
